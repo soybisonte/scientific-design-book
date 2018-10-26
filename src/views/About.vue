f5b29f3407cf16ddd0e22e855bb75e8267d3ac0a
Hector Heriberto Rodriguez Martinez <mb82922@MacBook-Pro.local>
2018-10-26 00:03:15 -0500

    remove background

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
+<script>
+// @ is an alias to /src
+// import { TimelineMax } from 'gsap';
+import BackgroundPattern from '@/components/BackgroundPattern.vue';
+import AppButton from '@/components/AppButton.vue';
+
+export default {
+  name: 'About',
+  components: {
+    BackgroundPattern,
+    AppButton,
+  },
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+  },
+};
+</script>
 <template>
   <div class="about">
+    <!-- <background-pattern>
+    </background-pattern> -->
     <h1>This is an about page</h1>
+    <app-button>boton</app-button>
   </div>
 </template>
